aa02e49ff61874414cfbf806560d3c1f6beefe5e
Philipp Malkov <phil.malkov@akvelon.com>
2020-10-22 22:58:56 +0300

    dotenv

--- db/db.js
+++ db/db.js
@@ -1,14 +1,17 @@
+require('../dotenv'); // eslint-disable-line import/order
+
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const { Dymon, StatsDChannel } = require('dymon');
 const dy2json = require('./dy2json');
 
 const DB = new DynamoDB({
   apiVersion: '2012-08-10',
+  endpoint: process.env.AWS_ENDPOINT || undefined,
   credentials: {
-    accessKeyId: 'AKIAZM7CQSNGXDQUY6PA',
-    secretAccessKey: '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',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  region: 'eu-central-1',
+  region: process.env.AWS_REGION,
 });
 
 const dymon = new Dymon({
